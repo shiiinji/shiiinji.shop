87401dd5d0e00c09b14f7ad9a276db7efe5972a5
Shinji Teramoto <shinji.toys@gmail.com>
2021-10-10 15:25:22 +0900

    feat: shop business logic

--- server/src/shop/shop.service.ts
+++ server/src/shop/shop.service.ts
@@ -1,4 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { Shop, Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
-export class ShopService {}
+export class ShopService {
+  constructor(private prisma: PrismaService) {}
+
+  async shop(
+    shopWhereUniqueInput: Prisma.ShopWhereUniqueInput,
+  ): Promise<Shop | null> {
+    return this.prisma.shop.findUnique({
+      where: shopWhereUniqueInput,
+    });
+  }
+
+  async shops(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.ShopWhereUniqueInput;
+    where?: Prisma.ShopWhereInput;
+    orderBy?: Prisma.ShopOrderByWithRelationInput;
+  }): Promise<Shop[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.shop.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async createShop(data: Prisma.ShopCreateInput): Promise<Shop> {
+    return this.prisma.shop.create({
+      data,
+    });
+  }
+
+  async updateShop(params: {
+    where: Prisma.ShopWhereUniqueInput;
+    data: Prisma.ShopUpdateInput;
+  }): Promise<Shop> {
+    const { where, data } = params;
+    return this.prisma.shop.update({
+      data,
+      where,
+    });
+  }
+
+  async deleteUser(where: Prisma.ShopWhereUniqueInput): Promise<Shop> {
+    return this.prisma.shop.delete({
+      where,
+    });
+  }
+}
