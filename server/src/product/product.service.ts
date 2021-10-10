a29e526ece1338d9b9690349b4d28bd426ae5792
Shinji Teramoto <shinji.toys@gmail.com>
2021-10-10 16:07:04 +0900

    feat: product business logic

--- server/src/product/product.service.ts
+++ server/src/product/product.service.ts
@@ -1,4 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { Product, Prisma } from '@prisma/client';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(private prisma: PrismaService) {}
+  async product(
+    productWhereUniqueInput: Prisma.ProductWhereUniqueInput,
+  ): Promise<Product | null> {
+    return this.prisma.product.findUnique({
+      where: productWhereUniqueInput,
+    });
+  }
+
+  async products(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.ProductWhereUniqueInput;
+    where?: Prisma.ProductWhereInput;
+    orderBy?: Prisma.ProductOrderByWithRelationInput;
+  }): Promise<Product[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.product.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async createProduct(data: Prisma.ProductCreateInput) {
+    return this.prisma.product.create({
+      data,
+    });
+  }
+
+  async updateProduct(params: {
+    where: Prisma.ProductWhereUniqueInput;
+    data: Prisma.ProductUpdateInput;
+  }): Promise<Product> {
+    const { data, where } = params;
+    return this.prisma.product.update({
+      data,
+      where,
+    });
+  }
+
+  async deleteProduct(where: Prisma.ProductWhereUniqueInput): Promise<Product> {
+    return this.prisma.product.delete({
+      where,
+    });
+  }
+}
