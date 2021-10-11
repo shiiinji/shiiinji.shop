8efb5ff7af36618adcc538a9a5b7d1d3bbe36a31
Shinji Teramoto <shinji.toys@gmail.com>
2021-10-12 00:17:00 +0900

    feat: shop/product services to app provider

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ShopService } from './shop/shop.service';
+import { ProductService } from './product/product.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ShopService, ProductService],
 })
 export class AppModule {}
