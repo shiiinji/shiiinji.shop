d105102d9ddd48aad3c09cae476e34f9271ce69f
Shinji Teramoto <shinji.toys@gmail.com>
2021-10-12 00:20:31 +0900

    feat: graphql module to app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShopService } from './shop/shop.service';
 import { ProductService } from './product/product.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      debug: true,
+      playground: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, ShopService, ProductService],
 })
