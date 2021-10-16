463a63dacd5c675a3acc068fcaf0657f2984e547
Shinji Teramoto <shinji.toys@gmail.com>
2021-10-17 01:43:42 +0900

    update: main modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ShopService } from './shop/shop.service';
-import { ProductService } from './product/product.service';
+import { PrismaModule } from './prisma.module';
+import { ShopModule } from './shop/shop.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
+      installSubscriptionHandlers: true,
+      autoSchemaFile: 'schema.gql',
       debug: true,
       playground: true,
     }),
+    PrismaModule,
+    ShopModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, ShopService, ProductService],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -4,12 +4,17 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+
+  const prismaService: PrismaService = app.get(PrismaService);
+  prismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
 }
 bootstrap();
