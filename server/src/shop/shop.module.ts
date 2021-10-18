30e9c0dbb6342849475addda28415ecd811c3802
Shinji Teramoto <shinji.toys@gmail.com>
2021-10-19 01:16:02 +0900

    remove: unused file

--- server/src/shop/shop.module.ts
+++ server/src/shop/shop.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ShopService } from './shop.service';
 import { ShopResolver } from './shop.resolver';
 import { PrismaModule } from '../prisma.module';
 
